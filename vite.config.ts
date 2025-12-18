bc9fd62e7b48095e8f17c975950ad430081a1303
youleyuan15-arch <youleyuan15@gmail.com>
2025-12-18 23:28:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-     base: '/cosmos-xmas-tree/',
+      base: '/cosmos-xmas-tree/', // 保持仓库名一致
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -17,7 +16,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': '/', // 简化别名映射
         }
       }
     };
